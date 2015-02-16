a595d9c22a1e0fba5a363b78606fd3fc45cc0dd7
duogeek <duogeek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-16 10:55:52 +0000

    js error fixed
    git-svn-id: https://plugins.svn.wordpress.org/duofaq-responsive-flat-simple-faq/trunk@1091228 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- duo-faq.php
+++ duo-faq.php
@@ -3,7 +3,7 @@
 Plugin Name: duoFAQ - Responsive, Flat, Simple FAQ
 Plugin URI: http://duogeek.com
 Description: A responsive and lightweight FAQ (Frequently Asked Questions) plugin by duogeek
-Version: 1.3.1
+Version: 1.3.2
 Author: duogeek
 Author URI: http://duogeek.com
 License: GPL v2 or later
